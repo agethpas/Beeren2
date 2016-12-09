2e19034e81874f81d84a38d0dd3ab5f7369d4633
Pascal <agethpas@students.zhaw.ch>
2016-12-09 11:43:22 +0100

    Info to dial emergency

--- app/src/main/java/com/afemsc/beeren/BerryInfo.java
+++ app/src/main/java/com/afemsc/beeren/BerryInfo.java
@@ -1,9 +1,17 @@
 package com.afemsc.beeren;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BerryInfo extends AppCompatActivity {
+
+
+    TextView infocall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,20 @@ public class BerryInfo extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berry_info);
+
+        infocall=(TextView) findViewById(R.id.info2);
+        infocall.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:145"));
+                startActivity(callIntent);
+            }
+        });
+
+
+
     }
 }
